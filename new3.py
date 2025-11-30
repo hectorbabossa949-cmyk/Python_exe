6497c09b5a2c2af86c468d9bda915c412707aefc
Min Thu Swe <hectorbabossa@gmail.com>
2025-11-30 11:52:42 +0630

    wow

---
+++ new3.py
@@ -0,0 +1,9 @@
+myan = input("Enter myanmar mark \n")
+eng = input("Enter english mark \n")
+maths = input("Enter Maths mark \n")
+sum = int(myan)+int(eng)+int(maths)
+avg = sum/3
+print("-"*28)
+print("total mark" +str(sum))
+print("Average mark:"+ str(avg))
+print("-"*28)
