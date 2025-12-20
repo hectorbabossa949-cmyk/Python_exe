fbc420a73b5a2987f398935ebea2b2aaaea70be1
Min Thu Swe <hectorbabossa@gmail.com>
2025-12-20 11:44:50 +0630

    Graphic

---
+++ graphicc.py
@@ -0,0 +1,19 @@
+import turtle
+
+screen = turtle.Screen()
+screen.bgcolor("blue")
+
+
+t = turtle.Turtle()
+t.color("white")
+t.shape("turtle")
+t.speed(1)
+
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+screen.mainloop()
